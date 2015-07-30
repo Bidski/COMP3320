2953bc68ff1935d672756ae01090d2119ef24b54
Alex Biddulph <c3124185@uon.edu.au>
2015-07-30 10:18:11 +0800

    Updated version of shader source files.

--- polygons/colours/fragmentShader.glsl
+++ polygons/colours/fragmentShader.glsl
@@ -1,11 +1,11 @@
-#version 150
+#version 150 core
 
-uniform vec3 triangleColour;
+in vec3 Colour;
 
 out vec4 outColour;
 
 void main()
 {
-    outColour = vec4(triangleColour, 1.0);
+    outColour = vec4(Colour, 1.0);
 }
 
